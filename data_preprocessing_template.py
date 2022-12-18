b83ae138874cbd4c5d77683c002f5f939ddc8cb4
gkamprem-innive <pkarthikeyan@inniveinc.com>
2022-12-18 15:31:31 +0530

    New Features

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -9,5 +9,7 @@ import pandas as pd
 city = ['Spain','India']
 salary = [10000,20000]
 Age = [17, 20]
-dataset = pd.read_excel('country.xlsx')
-dataset.to_csv("country1.xlsx")
+dataset = pd.read_csv('Data.csv')
+dataset.to_excel('country.xlsx')
+dataset = pd.read_excel("country.xlsx")
+dataset.to_csv('country2.csv')
